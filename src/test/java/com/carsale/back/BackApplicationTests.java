8f8535927fa262a66b099b388e903fafda7fa375
Seesiz <108148026+Seesiz@users.noreply.github.com>
2024-01-06 16:21:12 +0300

    Initial commit

---
+++ src/test/java/com/carsale/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsale.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
