e81b85f208f53bcdb9b67a88a18d81cef4266401
randy201 <108166894+randy201@users.noreply.github.com>
2024-01-12 10:53:14 +0300

    k,cdzoksn dozn q

--- src/main/java/com/carsale/back/API/Repository/VoitureRepository.java
+++ src/main/java/com/carsale/back/API/Repository/VoitureRepository.java
@@ -1,6 +1,7 @@
 package com.carsale.back.API.Repository;
 
 import com.carsale.back.API.Model.Categorie;
+import com.carsale.back.API.Model.EtatVoiture;
 import com.carsale.back.API.Model.Model;
 import com.carsale.back.API.Model.Voiture;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,5 @@ import java.util.List;
 public interface VoitureRepository extends JpaRepository<Voiture,Integer> {
     List<Voiture> findByCategorie(Categorie c);
     List<Voiture> findByModel(Model m);
+    List<Voiture> findByEtat(EtatVoiture EtatVoiture);
 }
