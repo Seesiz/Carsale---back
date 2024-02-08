1e176ccb0d620c6078440d4d190134e219f87dff
Seesiz <108148026+Seesiz@users.noreply.github.com>
2024-02-08 22:47:52 +0300

    cross origin

--- src/main/java/com/carsale/back/API/Controller/PushNotificationController.java
+++ src/main/java/com/carsale/back/API/Controller/PushNotificationController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class PushNotificationController {
 
 
